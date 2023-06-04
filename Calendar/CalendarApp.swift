3a4309851d8436da72fe23812b2c3c0bce1039e0
DARASKdot <102633287+DARASKdot@users.noreply.github.com>
2023-06-04 21:37:58 +0900

    Initial Commit

---
+++ Calendar/CalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalendarApp.swift
+//  Calendar
+//
+//  Created by Hiroshi Inoue on R 5/06/04.
+//
+
+import SwiftUI
+
+@main
+struct CalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
